bc7a8d17ac152cbfc8a9de5d2c741bad9511643a
Luigi Mangiacapra <luigimangiacapra@MacBook-Pro-di-Luigi.local>
2021-11-05 23:32:11 +0100

    Load route user-detail

---
+++ source/controllers/user-detail.controller.ts
@@ -0,0 +1,104 @@
+import { Request, Response, NextFunction } from "express";
+import { PrismaClient, DettaglioUtente} from '@prisma/client';
+
+import logging from "../config/logging";
+
+import IUserDetail from "../interfaces/user-detail.interface";
+import IJwtToken from "../interfaces/jwt-token.interface";
+
+const NAMESPACE = 'Users Details Controller';
+const prisma = new PrismaClient();
+
+const create = (req: Request, res: Response, next: NextFunction) => {
+    logging.debug(NAMESPACE, 'Creating user detail');
+
+    let bodyInfo: DettaglioUtente = req.body;
+
+    const dettaglioUente = prisma.dettaglioUtente.create({
+        data: bodyInfo
+    });
+
+    dettaglioUente.then(result => {
+        return res.status(200).json(result);
+    }).catch(error => {
+        logging.error(NAMESPACE, error.message, error);
+
+        return res.status(400).json({
+            message: error.message,
+            error
+        });
+    });
+};
+
+const usersDetails = (req: Request, res: Response, next: NextFunction) => {
+    logging.debug(NAMESPACE, 'Get all user details');
+
+    const userDetails = prisma.dettaglioUtente.findMany();
+
+    userDetails.then(result => {
+        return res.status(200).json(result);
+    }).catch(error => {
+        logging.error(NAMESPACE, error.message, error);
+
+        return res.status(400).json({
+            message: error.message,
+            error
+        });
+    });
+};
+
+const userDetail = (req: Request, res: Response, next: NextFunction) => {
+    logging.debug(NAMESPACE, 'Get single user detail');
+
+    let userDetailId = parseInt(req.params.userDetailId);
+
+    const userDetail = prisma.dettaglioUtente.findUnique({
+        where: {
+            IdDettaglioUtente: userDetailId
+        }
+    });
+
+    userDetail.then(result => {
+        return res.status(200).json(result);
+    }).catch(error => {
+        logging.error(NAMESPACE, error.message, error);
+
+        return res.status(400).json({
+            message: error.message,
+            error
+        });
+    });
+};
+
+const update = (req: Request, res: Response, next: NextFunction) => {
+    logging.debug(NAMESPACE, 'Update user detail');
+
+    let userDetailId = parseInt(req.params.userDetailId);
+
+    let bodyInfo: DettaglioUtente = req.body;
+
+    const userDetailUpdate = prisma.dettaglioUtente.update({
+        where: {
+            IdDettaglioUtente: userDetailId
+        },
+        data: bodyInfo
+    });
+
+    userDetailUpdate.then(result => {
+        return res.status(200).json(result);
+    }).catch(error => {
+        logging.error(NAMESPACE, error.message, error);
+
+        return res.status(400).json({
+            message: error.message,
+            error
+        });
+    });
+};
+
+export default {
+    create,
+    usersDetails,
+    userDetail,
+    update
+};
---
+++ source/interfaces/user-detail.interface.ts
@@ -0,0 +1,9 @@
+export default interface IUserDetail {
+    IdDettaglioUtente?: number,
+    Peso: string,
+    Altezza: string,
+    DataDettaglio: string,
+    Storico: number,
+    Utente_IdUtente: number,
+    Eliminato: number
+}
